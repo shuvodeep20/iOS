cc3a428a2e569afd5f3bd1e5afe87894f64eaf43
Shuvodeep Biswas <shuvodeep.20j@gmail.com>
2018-10-11 11:18:11 +0600

    Add files via upload

---
+++ basics/basics/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  basics
+//
+//  Created by Shuvodeep on 8/10/18.
+//  Copyright © 2018 ebsbd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    @IBAction func myButton(_ sender: Any) {
+        var text = ""
+        text = myTextField.text!
+        myLabel.text = "Hello there, \(text)!"
+        myTextField.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        myTextField.resignFirstResponder()
+        return false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
